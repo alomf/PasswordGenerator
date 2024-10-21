2c249d139bde82de3f92d5b38e857d5974fa3264
Fayham Alom <alomfn@gmail.com>
2024-10-21 11:32:38 +0100

    updated html and js files

--- index.js
+++ index.js
@@ -1,52 +1,36 @@
-let charactersList = [];
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
-const characters = [
-    "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
-];
+let password1 = "";
+let password2 = "";
+let generateBtn = document.getElementById("generate-btn");
+let password1El = document.getElementById("password1-el");
+let password2El = document.getElementById("password2-el");
 
-const charactersNumbers = [
-    "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
-    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
-];
+generateBtn.addEventListener("click", function generatedPassword() {
+    password1 = "";
+    password2 = "";
 
-const charactersSymbols = [
-    "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
-    "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+",
-    "=","{","[","}","]",",","|",":",";","<",">",".","?","/"
-];
-
-const charactersFull = [
-    "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
-    "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-    "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+",
-    "=","{","[","}","]",",","|",":",";","<",">",".","?","/"
-]
-
-let firstPasswordEl = document.getElementById("first-password");
-let secondPasswordEl = document.getElementById("second-password");
-let generatedPassword1 = "";
-let generatedPassword2 = "";
-
-function generate() {
-    generatedPassword1 = "";
-    firstPasswordEl.textContent = "";
-    generatedPassword2 = "";
-    secondPasswordEl.textContent = "";
-    for (let i = 0; i < 20; i++) {
-        let password = Math.floor(Math.random() * characters.length);
-        generatedPassword1 += characters[password];
-
-        firstPasswordEl.textContent = generatedPassword1;
+    for (let i = 0; i < 16; i++) {
+        let randomIndex = Math.floor(Math.random() * characters.length);
+        password1 += characters[randomIndex];
+        password1El.textContent = password1;
     }
-    
-    for (let i = 0; i < 20; i++) {
-        let password = Math.floor(Math.random() * charactersNumbers.length);
-        generatedPassword2 += charactersNumbers[password];
 
-        secondPasswordEl.textContent = generatedPassword2;
+    for (let i = 0; i < 16; i++) {
+        let randomIndex = Math.floor(Math.random() * characters.length);
+        password2 += characters[randomIndex];
+        password2El.textContent = password2;
+    }
+});
+
+function passwordCopy1() {
+    if (password1) {
+        window.prompt("Copy to clipboard: Ctrl+C, Enter", password1);
     }
 }
+
+function passwordCopy2() {
+    if (password2) {
+        window.prompt("Copy to clipboard: Ctrl+C, Enter", password2);
+    }
+};
